cd6b20ea347831f6b038edc07cfae902d427c703
Syr0kx <sven-spieler@web.de>
2016-01-22 15:12:16 +0100

    asdf

---
+++ User-Administratior/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace User_Administrator
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            test();
+        }
+        public void test()
+        {
+            try
+            {
+                MySqlConnection con = new MySqlConnection();
+                MySqlDataAdapter ad = new MySqlDataAdapter();
+                MySqlCommand cmd = new MySqlCommand();
+                string str = "SELECT Benutzername, Passwort FROM logindaten";
+                cmd.CommandText = str;
+                ad.SelectCommand = cmd;
+                con.ConnectionString = @"server=62.75.253.50;uid=WebBotIkariam;pwd=WebBotIkariam;database=WebBotIkariamAccounts;";
+                cmd.Connection = con;
+                DataTable dt = new DataTable();
+                ad.Fill(dt);
+                ListViewEmployeeDetails.DataContext = dt.DefaultView;
+                con.Close();
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show("Error 404:" + "\n" + e);
+            }
+        }
+
+        private void button_close_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button_add_Click(object sender, RoutedEventArgs e)
+        {
+            Boolean adding = true;
+            adduser add = new adduser(adding);
+            add.Owner = this;
+            add.Show();
+        }
+
+        private void button_delete_Click(object sender, RoutedEventArgs e)
+        {
+            Boolean adding = false;
+            adduser add = new adduser(adding);
+            add.Owner = this;
+            add.Show();
+        }
+    }
+}
---
+++ User-Administratior/manageuser.xaml.cs
@@ -0,0 +1,117 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace User_Administrator
+{
+    /// <summary>
+    /// Interaktionslogik für adduser.xaml
+    /// </summary>
+    public partial class adduser : Window
+    {
+        public Boolean adding;
+        public adduser(Boolean add)
+        {
+            InitializeComponent();
+            adding = add;
+            if (adding)
+            {
+                passwordBox.Visibility = Visibility.Visible;
+                button_add.Content = "add";
+                label_password.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                passwordBox.Visibility = Visibility.Hidden;
+                button_add.Content = "delete";
+                label_password.Visibility = Visibility.Hidden;
+            }
+        }
+
+        private void button_close_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        static string GetMd5Hash(MD5 md5Hash, string input)
+        {
+            byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+            StringBuilder sBuilder = new StringBuilder();
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+            return sBuilder.ToString();
+        }
+
+        private void button_add_Click(object sender, RoutedEventArgs e)
+        {
+            if (adding)
+            {
+                if (textBox_username.Text.Equals("") || passwordBox.Password.Equals(""))
+                {
+                    MessageBox.Show("Benutzername oder Passwort dürfen nicht leer sein !");
+                }
+                else
+                {
+                    using (MD5 md5Hash = MD5.Create())
+                    {
+                        MySqlConnection con = new MySqlConnection();
+                        MySqlDataAdapter ad = new MySqlDataAdapter();
+                        MySqlCommand cmd = new MySqlCommand();
+                        string str = "INSERT INTO logindaten(Benutzername,Passwort) VALUES('" + textBox_username.Text + "','" + GetMd5Hash(md5Hash, passwordBox.Password) + "');";
+                        cmd.CommandText = str;
+                        ad.SelectCommand = cmd;
+                        con.ConnectionString = @"server=62.75.253.50;uid=WebBotIkariam;pwd=WebBotIkariam;database=WebBotIkariamAccounts;";
+                        cmd.Connection = con;
+                        con.Open();
+                        cmd.ExecuteNonQuery();
+                        con.Close();
+                        ((MainWindow)Owner).test();
+                        this.Close();
+                    }
+                }
+            }
+            else // delete from database
+            {
+                if (textBox_username.Text.Equals(""))
+                {
+                    MessageBox.Show("Benutzername darf nicht leer sein !");
+                }
+                else
+                {
+                    using (MD5 md5Hash = MD5.Create())
+                    {
+                        MySqlConnection con = new MySqlConnection();
+                        MySqlDataAdapter ad = new MySqlDataAdapter();
+                        MySqlCommand cmd = new MySqlCommand();
+                        string str = "DELETE FROM logindaten Where Benutzername = '" + textBox_username.Text + "';";
+                        cmd.CommandText = str;
+                        ad.SelectCommand = cmd;
+                        con.ConnectionString = @"server=62.75.253.50;uid=WebBotIkariam;pwd=WebBotIkariam;database=WebBotIkariamAccounts;";
+                        cmd.Connection = con;
+                        con.Open();
+                        cmd.ExecuteNonQuery();
+                        con.Close();
+                        ((MainWindow)Owner).test();
+                        this.Close();
+                    }
+                }
+            }
+        }
+    }
+}
